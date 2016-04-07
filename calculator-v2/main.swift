b86134b331d91c7228f6e52c9f5d858abeff5c51
Brittney Hoy <brittneyhoy@Jonathans-Air.domain>
2016-04-07 00:19:38 -0700

    Added the array portion

--- calculator-v2/main.swift
+++ calculator-v2/main.swift
@@ -16,6 +16,7 @@ let subtract: (Int, Int) -> Int = { $0 - $1}
 let multiply: (Int, Int) -> Int = { $0 * $1}
 let divide: (Int, Int) -> Int = { $0 / $1}
 
+//test basic math functions
 print("Using 4 different functions")
 print("10 + 2 = \(add(10, 2))")
 print("10 - 2 = \(subtract(10, 2))")
@@ -29,8 +30,29 @@ func calculate (left : Int, right: Int, op : handleMathOp) -> Int{
     return op(left, right)
 }
 
+//test general math function
 print("Using generic math function!")
 print("10 + 2 = \(calculate(10, right: 2, op: add))")
 print("10 - 2 = \(calculate(10, right: 2, op: subtract))")
 print("10 * 2 = \(calculate(10, right: 2, op: multiply))")
 print("10 / 2 = \(calculate(10, right: 2, op: divide))")
+
+
+//arrays
+func addAll(values: [Int]) -> Int {
+    var total = 0
+    for value in values {
+        total += value
+    }
+    return total
+}
+
+func findAvg(values: [Int]) -> Int {
+    return addAll(values) / values.count
+}
+
+//test array functions
+let sampleArr = [1, 2, 3, 4, 5]
+print("Add all of the values in the array: \(addAll(sampleArr))")
+print("Find the average of the values in array: \(findAvg(sampleArr))")
+
